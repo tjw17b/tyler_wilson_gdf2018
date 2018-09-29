964396c2346244123bc2a37ae282da059495b843
Tyler Wilson <tjw17b@my.fsu.edu>
2018-09-28 22:31:26 -0400

    assignment 2 complete

---
+++ assignment2/Assets/playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PlayerController : MonoBehaviour
+{
+ 
+    private Rigidbody2D rb2d;
+ 
+    void start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+ 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce (movement);
+    }
+}
